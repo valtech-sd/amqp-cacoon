90fa07987b58617a990586ce8c03b958a099cd61
Daniel Morris <daniellmorris@gmail.com>
2020-05-14 10:59:52 -0500

    Add message batching to amqp-cacoon

--- readme.md
+++ readme.md
@@ -36,92 +36,116 @@ If using macos `brew cask install docker` should install both docker and docker-
    npm run test
    ```
 
-## Usage
+## Simple Usage
 
-### Connect and publish
+### Connect
+
+This allows you to connect to an amqp server
 
 ```javascript
-  const config = {
-    messageBus: {
-      // Protocol should be "amqp" or "amqps"
-      protocol: 'amqp',
-      // Username + Password on the RabbitMQ host
-      username: 'valtech',
-      password: 'iscool',
-      // Host
-      host: 'localhost',
-      // Port
-      port: 5672,
-      // Queue setup
-      testQueue: 'test-queue',
-    },
-  };
+const config = {
+  messageBus: {
+    // Protocol should be "amqp" or "amqps"
+    protocol: 'amqp',
+    // Username + Password on the RabbitMQ host
+    username: 'valtech',
+    password: 'iscool',
+    // Host
+    host: 'localhost',
+    // Port
+    port: 5672,
+    // Queue setup
+    testQueue: 'test-queue',
+  },
+};
 
-  let amqpCacoon = new AmqpCacoon({
-    protocol: config.messageBus.protocol,
-    username: config.messageBus.username,
-    password: config.messageBus.password,
-    host: config.messageBus.host,
-    port: config.messageBus.port,
-    amqp_opts: {},
-    providers: {
-      logger: logger,
-    },
-  });
-  let channel = await amqpCacoon.getPublishChannel(); // Connects and sets up a publish channel
-
-  // Create queue and setup publish channel
-  channel.assertQueue(config.messageBus.testQueue);
-
-  // Publish
-  await amqpCacoon.publish(
-    '', // Publish directly to queue
-    config.messageBus.testQueue,
-    Buffer.from('TestMessage')
-  );
+let amqpCacoon = new AmqpCacoon({
+  protocol: config.messageBus.protocol,
+  username: config.messageBus.username,
+  password: config.messageBus.password,
+  host: config.messageBus.host,
+  port: config.messageBus.port,
+  amqp_opts: {},
+  providers: {
+    logger: logger,
+  },
+});
 ```
 
-### Connect and Consume
+### Publish
+
+This allows you to publish a message
 
 ```javascript
-  const config = {
-    messageBus: {
-      // Protocol should be "amqp" or "amqps"
-      protocol: 'amqp',
-      // Username + Password on the RabbitMQ host
-      username: 'valtech',
-      password: 'iscool',
-      // Host
-      host: 'localhost',
-      // Port
-      port: 5672,
-      // Queue setup
-      testQueue: 'test-queue',
-    },
-  };
+let channel = await amqpCacoon.getPublishChannel(); // Connects and sets up a publish channel
 
-  let amqpCacoon = new AmqpCacoon({
-    protocol: config.messageBus.protocol,
-    username: config.messageBus.username,
-    password: config.messageBus.password,
-    host: config.messageBus.host,
-    port: config.messageBus.port,
-    amqp_opts: {},
-    providers: {
-      logger: logger,
-    },
-  });
+// Create queue and setup publish channel
+channel.assertQueue(config.messageBus.testQueue);
 
-  let channel = await amqpCacoon.getConsumerChannel(); // Connects and sets up a subscription channel
+// Publish
+await amqpCacoon.publish(
+  '', // Publish directly to queue
+  config.messageBus.testQueue,
+  Buffer.from('TestMessage')
+);
+```
 
-  // Create queue
-  channel.assertQueue(config.messageBus.testQueue);
+### Consume Single Message
 
-  // Consume
-  amqpCacoon.registerConsumer(
-    config.messageBus.testQueue,
-    async (channel: Channel, msg: ConsumeMessage) => {
-      console.log("Messsage content", msg.content.toString());
+This will allow use to consume a single message.
+
+```javascript
+let channel = await amqpCacoon.getConsumerChannel(); // Connects and sets up a subscription channel
+
+// Create queue
+channel.assertQueue(config.messageBus.testQueue);
+
+// Consume single message at a time
+amqpCacoon.registerConsumer(
+  config.messageBus.testQueue,
+  async (channel: Channel, msg: ConsumeMessage) => {
+    try {
+      console.log('Messsage content', msg.content.toString());
+      // ... Do other processing here
+      channel.ack(msg) // To ack a messages
+    } catch (e) {
+      channel.nack(msg) // To ack a messages
     }
-  );
+  }
+);
 ```
+
+### Consume Message Batch
+
+This allows you to wait until either some time has elapsed or a specified message size has been exceeded before the messages are consumed
+
+```javascript
+// Consume batch of message at a time. Configuration for time based or size based batching is provided
+amqpCacoon.registerConsumerBatch(
+  config.messageBus.testQueue,
+  async (channel: Channel, msg: ConsumeBatchMessages) => {
+    try {
+      console.log('Messsage content', msg.content.toString());
+      // ... Do other processing here
+      msg.ackAll() // To ack all messages
+    } catch (e) {
+      msg.nackAll() // To nack all messages
+    }
+  },
+  {
+    batching: {
+      maxTimeMs: 60000, // Don't provide messages to the callback until at least 60000 ms have passed
+      maxSizeBytes: 1024 * 1024 * 2, // 1024 * 1024 * 2 = 2 MB -don't provide message to the callback until 2 MB of data have been received
+    }
+  }
+);
+```
+
+## Dealing With Channels
+
+This library expose amqplib channel when you call either `getConsumerChannel` or `getPublishChannel`. The channel is also exposed when registering a consumer. To learn more about that api see documentation for [amqplib](https://www.npmjs.com/package/amqplib). Just a couple thing that you should remember to do.
+
+1. Remember to ack or nack on all messages.
+2. An alternative is to pass an option into the `registerConsumer` to not require an ack (noAck). The problem with this is that if your application is reset or errors out, you may loose the message or messages.
+
+
---
+++ src/helpers/message_batching_manager.ts
@@ -0,0 +1,220 @@
+import { Channel, ConsumeMessage, ConsumeBatchMessages } from '../index';
+import { Logger } from 'log4js';
+
+export interface IMessageBatchingManagerConfig {
+  providers: {
+    logger?: Logger;
+  };
+  maxSizeBytes?: number;
+  maxTimeMs?: number;
+  skipNackOnFail?: boolean;
+}
+
+export default class MessageBatchingManager {
+  private unackedMessageList: Array<ConsumeMessage> = [];
+  private bufferSize: number = 1;
+  private timerHandle?: NodeJS.Timeout;
+  private amqpChannel?: Channel;
+
+  constructor(public config: IMessageBatchingManagerConfig) {
+    this.resetMessages();
+  }
+
+  /**
+   * resetMessages
+   * Reset message list
+   * Do this by...
+   * 1. Reset unackedMessageList
+   * 2. Reset buffer size
+   *
+   * @param originalMessage: ConsumeMessage
+   * @param json: object
+   */
+  resetMessages() {
+    // 1. Reset unackedMessageList
+    this.unackedMessageList = [];
+    // 2. Reset buffer size (= 1 to account for end character. We account for comma and start character when adding to message)
+    this.bufferSize = 0;
+  }
+
+  /**
+   * addMessage
+   * Add message to list and buffer
+   * Do this by...
+   * 1. Push message to list
+   * 2. Increment buffer size
+   *
+   * @param originalMessage: ConsumeMessage
+   */
+  addMessage(msg: ConsumeMessage) {
+    // 1. Push message to list
+    this.unackedMessageList.push(msg);
+    // 2. Increment buffer size
+    this.bufferSize += msg.content.byteLength;
+  }
+
+  /**
+   * finalizeMessages
+   * Finalize message list
+   * Do this by...
+   * 1. Get current messages list
+   * 2. Reset message list
+   */
+  finalizeMessages(): {
+    bufferSize: number;
+    unackedMessageList: Array<ConsumeMessage>;
+  } {
+    // 1. Get current messages list
+    let unackedMessageList = this.unackedMessageList;
+    let bufferSize = this.bufferSize;
+
+    // 2. Reset message list
+    this.resetMessages();
+
+    return { bufferSize, unackedMessageList };
+  }
+
+  /**
+   * ackMessageList
+   * Ack all messages in list
+   * Do this by...
+   * 1. Loop over all messages in list and ack them
+   *
+   * @param channel: Channel - Channel
+   * @param messageList: Array<ConsumeMessage> - Messages to be acked
+   */
+  ackMessageList(
+    channel: Channel,
+    messageList: Array<ConsumeMessage>,
+    allUpTo?: boolean
+  ) {
+    // 1. Loop over all messages in list and ack them
+    for (let msg of messageList) {
+      channel.ack(msg, allUpTo);
+    }
+  }
+
+  /**
+   * nackMessageList
+   * Nack all messages in list
+   * Do this by...
+   * 1. Loop over all messages in list and nack them
+   *
+   * @param channel: Channel - Channel
+   * @param messageList: Array<ConsumeMessage> - Messages to be nacked
+   */
+  nackMessageList(
+    channel: Channel,
+    messageList: Array<ConsumeMessage>,
+    allUpTo?: boolean,
+    requeue?: boolean
+  ) {
+    // 1. Loop over all messages in list and nack them
+    for (let msg of messageList) {
+      channel.nack(msg, allUpTo, requeue);
+    }
+  }
+
+  /**
+   * sendBufferedMessages
+   * Send transformed buffered messages and ack/nack originals
+   * Do this by...
+   * 1. Finalize message buffer and fetch buffer and unackedMessageList
+   * 2. Call handler function (Assume it nacks or acks)
+   * 3. Ack unackedMessageList
+   * 4. If error then nack unackedMessageList. Assume nack never occured if here
+   *
+   * @returns void
+   */
+  async sendBufferedMessages(
+    channel: Channel,
+    handler: (channel: Channel, msg: ConsumeBatchMessages) => Promise<void>
+  ) {
+    let unackedMessageList: Array<ConsumeMessage> = [];
+    let bufferSize: number;
+    try {
+      // 1. Finalize message buffer and fetch buffer and unackedMessageList
+      ({ bufferSize, unackedMessageList } = this.finalizeMessages());
+
+      // 2. Send messages to handler
+      let messages: ConsumeBatchMessages = {
+        batchingOptions: {
+          maxTimeMs: this.config.maxTimeMs,
+          maxSizeBytes: this.config.maxSizeBytes,
+        },
+        totalSizeInBytes: bufferSize,
+        messages: unackedMessageList,
+        ackAll: (allUpTo?: boolean) =>
+          this.ackMessageList.bind(this, channel, unackedMessageList, allUpTo),
+        nackAll: (allUpTo?: boolean, requeue?: boolean) =>
+          this.nackMessageList.bind(
+            this,
+            channel,
+            unackedMessageList,
+            allUpTo,
+            requeue
+          ),
+      };
+      await handler(channel, messages);
+
+      // 3. Ack unackedMessageList
+      // this.ackMessageList(channel, unackedMessageList);
+    } catch (e) {
+      // 4. If error then nack unackedMessageList
+      if (!this.config.skipNackOnFail && unackedMessageList.length > 0) {
+        this.nackMessageList(channel, unackedMessageList);
+      }
+    }
+  }
+
+  /**
+   * handleMessageBuffering
+   * This function handles buffering and sending of messages
+   * The rules are as follows.
+   * 1. If buffer size > MAX_FILES_SIZE_BYTES then send the message
+   * 2. If time elapsed since first message in group > MAX_BUFFER_TIME_MS then send grouped messages
+   *
+   * Do this by...
+   * 1. Set channel variable for this object. This way we don't have to do an async call elswhere.
+   * 2. Add message to a buffer
+   * 3. Check buffer byte length and clear timer and send message if files size > MAX_FILES_SIZE_BYTES
+   * 4. Else Setup timer if it is not already setup to send buffered messages
+   * 5. When timer expires, send message
+   *
+   * @param originalMessage: ConsumeMessage - Used for ack and nack of messages
+   * @param transformedMessage: object - Sent in buffered message
+   * @returns void
+   */
+  async handleMessageBuffering(
+    channel: Channel,
+    msg: ConsumeMessage,
+    handler: (channel: Channel, msg: ConsumeBatchMessages) => Promise<void>
+  ) {
+    // 1. Set channel variable for this object. This way we don't have to do an async call elswhere.
+    this.amqpChannel = channel;
+    // 2. Add message to a buffer
+    this.addMessage(msg);
+    if (
+      this.config.maxSizeBytes &&
+      this.bufferSize >= this.config.maxSizeBytes
+    ) {
+      // 3. Check buffer byte length and clear timer and send message if files size > MAX_FILES_SIZE_BYTES
+      if (this.timerHandle) {
+        // Clear timeout
+        clearTimeout(this.timerHandle);
+        this.timerHandle = undefined;
+      }
+      // send message
+      await this.sendBufferedMessages(this.amqpChannel, handler);
+    } else if (this.config.maxTimeMs) {
+      // 4. Else Setup timer if it is not alread setup to send buffered messages
+      if (!this.timerHandle) {
+        this.timerHandle = setTimeout(() => {
+          // 5. When timer expires, send message. We know amqpChannel cannot be null here
+          this.sendBufferedMessages(this.amqpChannel!, handler);
+          this.timerHandle = undefined;
+        }, this.config.maxTimeMs);
+      }
+    }
+  }
+}
